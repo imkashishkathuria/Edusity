fd66ba16cad15b73eb26dc352a2f12966e8301d4
Kashish <kathuriak805@gmail.com>
2025-02-01 17:26:54 +0530

    completed programs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
+import Programs from "./components/Programs/Programs"
+import Title from "./components/Title/Title"
 
 
 const App = () => {
@@ -7,6 +9,11 @@ const App = () => {
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle={"OUR PROGRAM"} title={"What We Offer"} />
+        <Programs />
+      </div>
+     
     </div>
     
   )
