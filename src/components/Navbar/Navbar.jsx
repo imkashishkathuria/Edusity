f39a94de854488eade9c38ca8f6af70370c46268
Kashish <kathuriak805@gmail.com>
2025-02-04 22:48:16 +0530

    complete responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,16 @@ import logo from '../../assets/logo.png'
 import { useState } from 'react'
 import { useEffect } from 'react';
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
 
 const Navbar = () => {
   
   const [sticky, setSticky] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = () =>{
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+  }
 
   useEffect(()=>{
     window.addEventListener("scroll",()=>{
@@ -17,7 +23,7 @@ const Navbar = () => {
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt='' className='logo'/>
-      <ul>
+      <ul className={mobileMenu ? "": 'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -25,6 +31,7 @@ const Navbar = () => {
         <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
         <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'> Contact Us</Link></li>
       </ul>
+      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
